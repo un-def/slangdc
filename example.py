78dff076df82ee09659a633cc821467567def92c
un.def <un.def@ya.ru>
2014-10-27 20:26:06 +0300

    example.py: threads rearranging

--- example.py
+++ example.py
@@ -7,21 +7,34 @@ import re
 import slangdc
 
 settings = {
-    'address': 'ozerki.org',
-    #'nick': 'las.cifritas',
+    'address': 'allavtovo.ru',
+    #'nick': 'slangdc',
     #'password': 'psswrd',
     #'email': 'un.def@ya.ru',
     'desc': 'git',
     'slots': 10,
     'encoding': 'windows-1251',
-    'timeout': 120
+    'timeout': 300
 }
 
-class Printer(threading.Thread):
+class DCThread(threading.Thread):
 
     def __init__(self, dc):
         self.dc = dc
         threading.Thread.__init__(self)
+
+    def run(self):
+        self.dc.connect(get_nicks=True)
+        while self.dc.connected:
+            self.dc.receive(raise_exc=False)
+
+
+class PrintThread(threading.Thread):
+
+    def __init__(self, dc):
+        self.dc = dc
+        threading.Thread.__init__(self)
+        self.daemon = True
 
     def run(self):
         while True:
@@ -46,52 +59,47 @@ class Printer(threading.Thread):
                 timestamp = datetime.fromtimestamp(message['time']).strftime('[%H:%M:%S]')
                 print("{0} {1} {2}".format(timestamp, pref, message['text']))
             time.sleep(0.01)
-        print("@close Printer")
 
 
-class Typer(threading.Thread):
+class InputThread(threading.Thread):
 
     def __init__(self, dc):
         self.dc = dc
         threading.Thread.__init__(self)
+        self.daemon = True
 
     def run(self):
-        while self.dc.connected:
+        while True:
             message = input()
             if message:
-                pm = re.fullmatch('!!pm (.+?) (.+)', message)
+                pm = re.fullmatch('/pm (.+?) (.+)', message)
                 if pm:
                     dc.pm_send(pm.group(1), pm.group(2))
-                elif message == '!!quit':
+                elif message == '/quit':
                     self.dc.disconnect()
-                elif message == '!!showjoins':
+                    return
+                elif message == '/showjoins':
                     dc.showjoins = not dc.showjoins
                     print("showjoins: ", dc.showjoins)
-                elif message == '!!nickcount':
+                elif message == '/usercount':
                     print("users: ", len(dc.nicklist))
-                elif message == '!!nicklist':
+                elif message == '/nicklist':
                     for nmb, nick in enumerate(sorted(dc.nicklist), 1):
                         print("{0: 4d} {1}".format(nmb, nick))
-                elif message == '!!oplist':
+                elif message == '/oplist':
                     oplist_str = " ".join(dc.nicklist.ops) if dc.nicklist.ops else "none"
                     print("ops:", oplist_str)
-                elif message == '!!botlist':
+                elif message == '/botlist':
                     botlist_str = " ".join(dc.nicklist.bots) if dc.nicklist.bots else "none"
                     print("bots:", botlist_str)
                 else:
                     dc.chat_send(message)
-        print("@close Typer")
 
 
 dc = slangdc.DCClient(**settings)
-printer = Printer(dc)
-printer.start()
-
-dc.connect(get_nicks=True)
-if dc.connected:
-    typer = Typer(dc)
-    typer.start()
-    while dc.connected:
-        dc.receive(raise_exc=False)
+PrintThread(dc).start()
+InputThread(dc).start()
+dcthread = DCThread(dc)
+dcthread.start()
+dcthread.join()
 time.sleep(0.5)
-print("@close main")
