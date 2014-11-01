6c5b89881ded1d5d26021afa1b25dcce9232d3d2
un.def <un.def@ya.ru>
2014-11-01 21:22:27 +0300

    address entry

--- gui.py
+++ gui.py
@@ -8,20 +8,20 @@ class Wrapper:
 
     def __init__(self):
         self.settings = {
-            'address': 'allavtovo.ru',
-            #'nick': 'slangdc',
-            #'password': 'psswrd',
             'encoding': 'windows-1251',
             'timeout': 900
         }
         self.dc = None
 
-    def connect(self):
+    def connect(self, entry):
         if not self.dc or not self.dc.connecting:   # если ещё не подключались или не подключаемся в данный момент
-            self.disconnect()   # отключимся, если уже подключены
-            self.dc = slangdc.DCClient(**self.settings)
-            PrintThread(self.dc).start()
-            DCThread(self.dc).start()
+            address = entry.get().strip()
+            if address:
+                self.disconnect()   # отключимся, если уже подключены
+                self.settings['address'] = address
+                self.dc = slangdc.DCClient(**self.settings)
+                PrintThread(self.dc).start()
+                DCThread(self.dc).start()
 
     def disconnect(self):
         if self.dc:
@@ -58,11 +58,19 @@ class Wrapper:
 wrapper = Wrapper()
 root = Tk()
 root.title("slangdc.Tk")
-Button(root, text="connect", command=wrapper.connect).pack(side=LEFT)
-Button(root, text="disconnect", command=wrapper.disconnect).pack(side=LEFT)
-msg_entry = Entry(root, width=50)
+ftop = Frame(root)
+ftop.pack()
+address_entry = Entry(ftop)
+address_entry.insert(0, "allavtovo.ru")
+address_entry.pack(side=LEFT)
+address_entry.bind('<Return>', lambda e: wrapper.connect(address_entry))
+Button(ftop, text="connect", command=lambda: wrapper.connect(address_entry)).pack(side=LEFT)
+Button(ftop, text="disconnect", command=wrapper.disconnect).pack(side=LEFT)
+Button(ftop, text="quit", command=wrapper.quit).pack(side=LEFT)
+fbottom = Frame(root)
+fbottom.pack()
+msg_entry = Entry(fbottom, width=50)
 msg_entry.pack(side=LEFT)
 msg_entry.bind('<Return>', lambda e: wrapper.send(msg_entry))
-Button(root, text="send", command=lambda: wrapper.send(msg_entry)).pack(side=LEFT)
-Button(root, text="quit", command=wrapper.quit).pack(side=LEFT)
+Button(fbottom, text="send", command=lambda: wrapper.send(msg_entry)).pack(side=LEFT)
 root.mainloop()
